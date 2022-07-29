432565f6ebcfefd06ea89b2711db9d6f2b0cf58d
Elizabeth Pursell <emp520@lehigh.edu>
2022-07-29 15:42:22 -0400

    Add files via upload

---
+++ spec_set_nutrition_facts.js
@@ -0,0 +1,50 @@
+/**
+ *@NApiVersion 2.x
+ *@NScriptType ClientScript
+ */
+ define(['N/currentRecord', 'N/search'], function(currentRecord, search) {
+    function pageInit(context) {
+      	//TO DO
+    }
+    function fieldChanged(context) {
+        var itemRecord = context.currentRecord;
+        var fieldName = context.fieldId;
+        if(fieldName == 'custitemselect_flavor' || fieldName == 'custitemselect_weight'){
+          	var flavor = itemRecord.getValue({
+        		fieldId: 'custitemselect_flavor'
+        	});
+        	var weight = itemRecord.getValue({
+          		fieldId: 'custitemselect_weight'
+        	});
+            var bagSearch = search.load({
+                id: 'customsearchspfg_bag'
+            });
+            var flavorFilter = search.createFilter({
+                name: 'custrecordmarshmallow_flavor',
+                operator: search.Operator.IS,
+                values: [flavor]
+            });
+            var weightFilter = search.createFilter({
+                name: 'custrecordweight_settings',
+                operator: search.Operator.IS,
+                values: [weight]
+            });
+            var internalid = 0;
+            bagSearch.filters.push(flavorFilter);
+            bagSearch.filters.push(weightFilter);
+            bagSearch.run().each(function(result){
+                internalid = result.getValue({
+                    name: "internalid"
+                });
+      	    });
+            itemRecord.setValue({
+                fieldId: 'custitemnutrition_facts_select',
+                value: internalid
+            });
+        }
+    }
+    return {
+        pageInit: pageInit,
+        fieldChanged: fieldChanged
+    }
+});
