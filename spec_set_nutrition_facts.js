1f2db6ee46aaa8871c346f0ca4f81788a9a0e5f8
Elizabeth Pursell <emp520@lehigh.edu>
2022-08-01 13:12:07 -0400

    Update spec_set_nutrition_facts.js

--- spec_set_nutrition_facts.js
+++ spec_set_nutrition_facts.js
@@ -7,20 +7,20 @@
       	//TO DO
     }
     function fieldChanged(context) {
-        var itemRecord = context.currentRecord;
+        var itemRecord = context.currentRecord;				//get current record and field
         var fieldName = context.fieldId;
-        if(fieldName == 'custitemselect_flavor' || fieldName == 'custitemselect_weight'){
-          	var flavor = itemRecord.getValue({
-        		   fieldId: 'custitemselect_flavor'
-        	  });
-        	  var weight = itemRecord.getValue({
-          		 fieldId: 'custitemselect_weight'
-        	  });
-           if(flavor != null && flavor != '' && weight != null && weight != ''){
-             var bagSearch = search.load({
+        if(fieldName == 'custitemselect_flavor' || fieldName == 'custitemselect_weight'){		//executes if flavor or weight settings changed
+          	var flavor = itemRecord.getValue({			//get the current values
+        		fieldId: 'custitemselect_flavor'
+        	});
+        	var weight = itemRecord.getValue({
+          		fieldId: 'custitemselect_weight'
+        	});
+        	if(flavor != null && flavor != '' && weight != null && weight != ''){		//if both selections are not null
+            	var bagSearch = search.load({			//load search for all bag records
                 	id: 'customsearchspfg_bag'
             	});
-             var flavorFilter = search.createFilter({
+             	var flavorFilter = search.createFilter({		//create filters for flavor and weight settings
                 	name: 'custrecordmarshmallow_flavor',
                 	operator: search.Operator.IS,
                 	values: [flavor]
@@ -33,22 +33,22 @@
             	var internalid = 0;
             	bagSearch.filters.push(flavorFilter);
             	bagSearch.filters.push(weightFilter);
-            	bagSearch.run().each(function(result){
+            	bagSearch.run().each(function(result){			//run search; get internal ID of each result (should only be one result)
                 	internalid = result.getValue({
                     	name: "internalid"
                 	});
-      	    	 });
-          		 if(internalid == null || internalid == ''){
+      	    	});
+          		if(internalid == null || internalid == ''){				//set bag selection if option does not exist
               		itemRecord.setValue({
                 		fieldId: 'custitemnutrition_facts_select',
                 		value: 22
-            		  });
+            		});
             	}
-          		 else{
-              		itemRecord.setValue({
+          		else{
+              		itemRecord.setValue({								//set bag selection if option exists
                 		fieldId: 'custitemnutrition_facts_select',
                 		value: internalid
-            		  });
+            		});
             	}
             }
         }
