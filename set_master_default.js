ed56e6f05a7827eb1fd930d2dfa55fb8712aac42
Elizabeth Pursell <emp520@lehigh.edu>
2022-08-01 13:06:38 -0400

    Update set_master_default.js

--- set_master_default.js
+++ set_master_default.js
@@ -4,12 +4,12 @@
  */
  define(['N/currentRecord'], function(currentRecord) {
     function pageInit(context) {
-      	var itemRecord = context.currentRecord;
-        itemRecord.selectLine({
+      	var itemRecord = context.currentRecord;         //get current record
+        itemRecord.selectLine({                     //select first line in sublist
     		sublistId: 'billofmaterials',
           	line: 0
 		});
-      	itemRecord.setCurrentSublistValue({
+      	itemRecord.setCurrentSublistValue({         //set sublist's field value
         	sublistId: 'billofmaterials',
             fieldId: 'masterdefault',
             value: true
