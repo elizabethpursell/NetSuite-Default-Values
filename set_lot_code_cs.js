ccbd6fe52d29981c34a07f1b882d4d852048d160
Elizabeth Pursell <emp520@lehigh.edu>
2023-01-09 13:00:44 -0500

    Update set_lot_code_cs.js

--- set_lot_code_cs.js
+++ set_lot_code_cs.js
@@ -7,22 +7,35 @@
         if (context.mode !== 'create')          //only execute in create mode
             return;
         var currentRecord = context.currentRecord;
-        var currentDate = new Date();
-        var date = currentDate.getFullYear().toString();
-        date += (currentDate.getMonth() + 1).toString();
-        date += currentDate.getDate().toString();
-        currentRecord.selectLine({
-            sublistId: 'inventoryassignment',
-            line: 0
+        var itemName = currentRecord.getText({
+            fieldId: "item"
         });
-        currentRecord.setCurrentSublistValue({      //set date as lot number on new inventory details
-            sublistId: 'inventoryassignment',
-            fieldId: 'receiptinventorynumber',
-            value: date,
-            ignoreFieldChange: true
-        });
+        if(itemName.search(/SPFG/i) != -1 || itemName.search(/SPWIP/i) != -1){
+            var currentDate = new Date();
+            var date = currentDate.getFullYear().toString();
+            var month = currentDate.getMonth() + 1;
+            if(month < 10){
+                month = "0" + month.toString();
+            }
+            date += (month).toString();
+            var day = currentDate.getDate();
+            if(day < 10){
+                day = "0" + day.toString();
+            }
+            date += day.toString();
+            currentRecord.selectLine({
+                sublistId: 'inventoryassignment',
+                line: 0
+            });
+            currentRecord.setCurrentSublistText({      //set date as lot number on new inventory details
+                sublistId: 'inventoryassignment',
+                fieldId: 'receiptinventorynumber',
+                text: date,
+                ignoreFieldChange: true
+            });
+        }
     }
     return {
         pageInit: pageInit
     }
-})
+});
